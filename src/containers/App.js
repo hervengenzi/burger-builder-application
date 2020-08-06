bd454af5821226ca995feffba6b9c7e356c8b40b
hervengenzi <ngenziherve@octangrp.com>
2020-08-06 14:33:09 +0200

    Change font family and include fonts into our index.html folder

---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/Layout/Layout';
+
+function App() {
+  return (
+    <div>
+      <Layout>
+        <p>Here we are</p>
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
