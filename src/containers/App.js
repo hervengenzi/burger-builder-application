1f9d92625c807f3e90759d526e48970dcd84b4f8
hervengenzi <ngenziherve@octangrp.com>
2020-08-06 15:48:33 +0200

    Add prop type validation

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Layout from '../components/Layout/Layout';
+import BurgerBuilder from './BurgerBuilder/BurgerBuilder';
 
 function App() {
   return (
     <div>
       <Layout>
-        <p>Here we are</p>
+        <BurgerBuilder />
       </Layout>
     </div>
   );
