dfeb25f4d3852083b56d49c98419aa3ac375b80a
hervengenzi <herve@octan.group>
2020-09-03 08:31:48 +0200

    use update object in auth

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -48,9 +48,9 @@ export const auth = (email, password, isSignup) => {
             password: password,
             returnSecureToken: true
         };
-        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyB5cHT6x62tTe-g27vBDIqWcwQWBSj3uiY';
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAEFu9Lu-ipQ2pHIoVwIvjDhx6kjeREHyw';
         if (!isSignup) {
-            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyB5cHT6x62tTe-g27vBDIqWcwQWBSj3uiY';
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:verifyPassword?key=AIzaSyAEFu9Lu-ipQ2pHIoVwIvjDhx6kjeREHyw';
         }
         axios.post(url, authData)
             .then(response => {
