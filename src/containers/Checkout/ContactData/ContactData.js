58f161b977fff594a44ee7138c0325366716ad8b
hervengenzi <herve@octan.group>
2020-09-03 12:26:30 +0200

    update form element

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -118,7 +118,7 @@ class ContactData extends Component {
     inputChangedHandler = (event, inputIdentifier) => {
         const updatedFormElement = updateObject(this.state.orderForm[inputIdentifier], {
             value: event.target.value,
-            valid:  checkValidity(updatedFormElement.value, this.state.validation),
+            valid:  checkValidity(updatedFormElement, this.state.validation),
             touched: true
         });
 
